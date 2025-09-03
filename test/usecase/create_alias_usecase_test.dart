aa0d4fce89e983d43933f04ebf4c6b6cee62fddc
Pedro <pedrohsbelfort@gmail.com>
2025-09-03 20:14:27 -0300

    Add createAlias function and corresponding tests for alias creation functionality

---
+++ test/usecase/create_alias_usecase_test.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:link_app/data/remote/alias_remote.dart';
+import 'package:link_app/domain/entities/alias_entity.dart';
+import 'package:link_app/domain/repositories/alias/alias_repository.dart';
+import 'package:link_app/domain/usecases/create_alias_usecase.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'create_alias_usecase_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AliasRemote>()])
+void main() {
+  late final MockAliasRemote mockAliasRemote;
+  late final AliasRepository aliasRepository;
+  late final CreateAliasUseCase createAliasUseCase;
+
+  setUp(() {
+    mockAliasRemote = MockAliasRemote();
+    aliasRepository = AliasRepository(mockAliasRemote);
+    createAliasUseCase = CreateAliasUseCase(aliasRepository);
+  });
+
+  test('when request is successful', () async {
+    when(mockAliasRemote.createAlias('www.google.com')).thenAnswer(
+      (_) async => Right(
+        AliasEntity(
+          alias: '702889725',
+          original: 'www.google.com',
+          short:
+              'https://url-shortener-server.onrender.com/api/alias/702889725',
+        ),
+      ),
+    );
+
+    final result = await createAliasUseCase.call('www.google.com');
+    expect(result, isA<AliasEntity>());
+  });
+}
